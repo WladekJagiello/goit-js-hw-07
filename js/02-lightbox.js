b912c7d1fc0374f7432592df7bdde135e483695e
Vadym <120068188+WladekJagiello@users.noreply.github.com>
2023-05-07 16:16:38 +0300

    ...

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryList = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+galleryEl.innerHTML = galleryList;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+});
