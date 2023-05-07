4c2e1e8851898af56c3b23b06d4c192319011b42
Vadym <120068188+WladekJagiello@users.noreply.github.com>
2023-05-07 14:41:10 +0300

    ...

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryList = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+function clickItem(event) {
+  event.preventDefault();
+
+  function pressEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", pressEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", pressEsc);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+galleryEl.innerHTML = galleryList;
+galleryEl.addEventListener("click", clickItem);
